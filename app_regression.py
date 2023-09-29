3191be1a49b9bd931f5ce0673f1c0f5c8ce84835
Asamaporn Pladsri <68784579+Jajahz@users.noreply.github.com>
2023-09-29 22:39:34 +0700

    Add files via upload

--- app_regression.py
+++ app_regression.py
@@ -1,7 +1,6 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
@@ -9,33 +8,27 @@ import joblib
 model_filename = 'linear_regression_model.pkl'
 loaded_model = joblib.load(model_filename)
 
-# Assuming 'Walmart.csv' is in the same directory as your Python script
-file_path = 'Walmart.csv'
+# Example DataFrame for prediction
+example_data = pd.DataFrame({
+    'Store': [2],
+    'Holiday_Flag': [1],
+    'Temperature': [75.0],
+    'Fuel_Price': [2.5],
+    'CPI': [140.0],
+    'Unemployment': [6.5]
+})
 
-# Read the CSV file into a pandas DataFrame
-df = pd.read_csv(file_path)
-
-# Features (X) and Target (y)
-features = df[['Store', 'Holiday_Flag', 'Temperature', 'Fuel_Price', 'CPI', 'Unemployment']]
-target = df['Weekly_Sales']
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-
-st.title('Weekly Sales Prediction App')
-
-st.header('Input Features for Prediction')
-
-# User input for prediction
-store = st.number_input('Store', min_value=1)
-holiday_flag = st.selectbox('Holiday Flag', [0, 1])
-temperature = st.number_input('Temperature', min_value=-100.0, max_value=100.0, value=75.0)
-fuel_price = st.number_input('Fuel Price', min_value=0.0, value=2.5)
-cpi = st.number_input('CPI', min_value=0.0, value=140.0)
-unemployment = st.number_input('Unemployment', min_value=0.0, value=6.5)
+# Sidebar for user input
+st.sidebar.header('Input Features for Prediction')
+store = st.sidebar.number_input('Store', min_value=1)
+holiday_flag = st.sidebar.selectbox('Holiday Flag', [0, 1])
+temperature = st.sidebar.number_input('Temperature', min_value=-100.0, max_value=100.0, value=75.0)
+fuel_price = st.sidebar.number_input('Fuel Price', min_value=0.0, value=2.5)
+cpi = st.sidebar.number_input('CPI', min_value=0.0, value=140.0)
+unemployment = st.sidebar.number_input('Unemployment', min_value=0.0, value=6.5)
 
 # Create a DataFrame with user input
-example_data = pd.DataFrame({
+user_data = pd.DataFrame({
     'Store': [store],
     'Holiday_Flag': [holiday_flag],
     'Temperature': [temperature],
@@ -45,19 +38,19 @@ example_data = pd.DataFrame({
 })
 
 # Make predictions using the loaded model
-predicted_sales = loaded_model.predict(example_data)
+predicted_sales = loaded_model.predict(user_data)
 
 # Display predicted sales
 st.subheader('Predicted Weekly Sales:')
 st.write(f'{predicted_sales[0]:.2f}')
 
+# (Assuming y_test and predictions are defined in your original code)
 # Calculate Mean Squared Error (MSE)
-mse = mean_squared_error(y_test, loaded_model.predict(X_test))
+# mse = mean_squared_error(y_test, predictions)
+# st.subheader('Mean Squared Error (MSE):')
+# st.write(f'{mse:.2f}')
 
-# Calculate R-squared (R²)
-r_squared = r2_score(y_test, loaded_model.predict(X_test))
-
-# Display MSE and R-squared
-# st.subheader('Model Evaluation Metrics:')
-# st.write(f'Mean Squared Error (MSE): {mse:.2f}')
-# st.write(f'R-squared (R²): {r_squared:.2f}')
+# # Calculate R-squared (R²)
+# r_squared = r2_score(y_test, predictions)
+# st.subheader('R-squared (R²):')
+# st.write(f'{r_squared:.2f}')
